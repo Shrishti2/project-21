b9fb40a39d304124962cf1793dd32bf530638ed3
Shrishti2 <76557189+Shrishti2@users.noreply.github.com>
2021-01-11 18:45:54 +0530

    Add files via upload

---
+++ my.library.js
@@ -0,0 +1,26 @@
+function isTouching(car,wall){
+    if (car.x - wall.x < wall.width/2 + car.width/2
+        && wall.x - car.x < wall.width/2 + car.width/2
+        && car.y - wall.y < wall.height/2 + car.height/2
+        && wall.y - car.y < wall.height/2 + car.height/2) {
+          return true;
+    }
+    else {
+          return false;
+    }
+  
+  }
+  
+  function bounceOff(car,wall){
+    if (car.x - wall.x < wall.width/2 + car.width/2
+      && wall.x - car.x < wall.width/2 + car.width/2) {
+        car.velocityX = car.velocityX * (-1);
+        wall.velocityX = wall.velocityX * (-1);
+    }
+    if (car.y - wall.y < wall.height/2 + car.height/2
+    && wall.y - car.y < wall.height/2 + car.height/2){
+      car.velocityY = car.velocityY * (-1);
+      wall.velocityY = wall.velocityY * (-1);
+    }
+  
+  }
